1b15550a534bb54c5fa76cbaa2876cccd14d98ba
Domino5740 <domino5740@o2.pl>
2020-08-17 12:37:23 +0200

    really basic car control

--- car_ctl.v
+++ car_ctl.v
@@ -23,45 +23,93 @@
 module car_ctl(
     input wire pclk,
     input wire rst,
+    input wire frame_ended,
     input wire [3:0] key,
     output reg [10:0] xpos,
     output reg [10:0] ypos
 );
 
-localparam SPEED_MAX = 2137;
-localparam SPEED_MIN = -2137;
+localparam SPEED_MAX = 2000;
+localparam SPEED_MIN = -2000;
 localparam ARROW_UP = 4'b0001;
+localparam ARROW_DOWN = 4'b0010;
+localparam ARROW_LEFT = 4'b0100;
+localparam ARROW_RIGHT = 4'b1000;
+localparam ARROW_UPLEFT = ARROW_UP | ARROW_LEFT;
+localparam ARROW_UPRIGHT = ARROW_UP | ARROW_RIGHT;
+localparam ARROW_DOWNLEFT = ARROW_DOWN | ARROW_LEFT;
+localparam ARROW_DOWNRIGHT = ARROW_DOWN | ARROW_RIGHT;
 
 reg [10:0] xpos_nxt, ypos_nxt;
-reg [10:0] speed, speed_nxt;
+signed reg [10:0] xspeed, xspeed_nxt, yspeed, yspeed_nxt;
 reg [10:0] timer, timer_nxt;
 
 always @(posedge pclk)
 if(rst)
 begin
-    xpos <= 0;
-    ypos <= 0;
+    xpos <= 640;
+    ypos <= 480;
+    xspeed <= 0;
+    yspeed <= 0;
     timer <= 0;
 end
 else
 begin
     xpos <= xpos_nxt;
     ypos <= ypos_nxt;
+    xspeed <= xspeed_nxt;
+    yspeed <= yspeed_nxt;
     timer <= timer_nxt;
 end
 
-always @*
+always @(frame_ended)
 begin
-    speed_nxt = speed;
     xpos_nxt  = xpos + speed;
-    ypos_nxt  = ypos;
+    ypos_nxt  = ypos + speed;
+    xspeed_nxt = xspeed - 2;
+    yspeed_nxt = yspeed - 2;
     case(key)
         ARROW_UP:
         begin
-            speed_nxt = speed + 1;
-            xpos_nxt  = xpos + speed;
+            yspeed_nxt = yspeed + 5;
+        end
+        ARROW_DOWN:
+        begin
+            yspeed_nxt = yspeed - 5;
+        end
+        ARROW_RIGHT:
+        begin
+            xspeed_nxt = xspeed + 5;
+        end
+        ARROW_LEFT:
+        begin
+            xspeed_nxt = xspeed - 5;
+        end
+        ARROW_UPLEFT:
+        begin
+            xspeed_nxt = xspeed - 5;
+            yspeed_nxt = yspeed + 5;
+        end
+        ARROW_UPRIGHT:
+        begin
+            xspeed_nxt = xspeed + 5;
+            yspeed_nxt = yspeed + 5;
+        end
+        ARROW_DOWNLEFT:
+        begin
+            xspeed_nxt = xspeed - 5;
+            yspeed_nxt = yspeed - 5;
+        end
+        ARROW_DOWNRIGHT:
+        begin
+            xspeed_nxt = xspeed + 5;
+            yspeed_nxt = yspeed - 5;
         end
     endcase
+    if(xspeed >= SPEED_MAX) xspeed = SPEED_MAX;
+    else if (xspeed <= SPEED_MIN) xspeed = SPEED_MIN;
+    if(yspeed >= SPEED_MAX) yspeed = SPEED_MAX;
+    else if (yspeed <= SPEED_MIN) yspeed = SPEED_MIN;
 end
 
 endmodule
