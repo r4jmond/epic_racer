436ae723e3be80854db255a097c842916077cc38
Domino5740 <domino5740@o2.pl>
2020-09-08 18:29:34 +0200

    basic collisions with surroundings

--- car_ctl.v
+++ car_ctl.v
@@ -18,6 +18,7 @@ localparam KEY_UP = 4'b0001;
 localparam KEY_DOWN = 4'b0010;
 localparam KEY_LEFT = 4'b0100;
 localparam KEY_RIGHT = 4'b1000;
+
 /*
 localparam KEY_UPLEFT = KEY_UP | KEY_LEFT;
 localparam KEY_UPRIGHT = KEY_UP | KEY_RIGHT;
@@ -30,15 +31,14 @@ localparam GOING_DOWN = 3'b011;
 localparam GOING_LEFT = 3'b010;
 localparam GOING_RIGHT = 3'b110;
 */
+
 localparam DIR_DOWN = 2'b00;
 localparam DIR_RIGHT = 2'b01;
 localparam DIR_LEFT = 2'b11;
 localparam DIR_UP = 2'b10;
 
-
-
 localparam DELAY_MIN = 100000;
-localparam DELAY_STEP = 10000;
+localparam DELAY_STEP = 5000;
 localparam DELAY_SLOWED = 350000;
 localparam DELAY_MAX = 400000;
 
@@ -47,15 +47,17 @@ reg [2:0] state, state_nxt;
 reg [23:0] xtimer, xtimer_nxt, ytimer, ytimer_nxt;
 reg [23:0] xdelay, xdelay_nxt, ydelay, ydelay_nxt;
 reg [1:0] move_dir_nxt;
+reg [10:0] car_x_pos, car_y_pos;
+
 
 always @(posedge pclk)
 if(rst)
 begin
-    xpos <= 300;
-    ypos <= 250;
+    xpos <= 490;
+    ypos <= 90;
     xtimer <= 0;
     ytimer <= 0;
-    move_dir <= DIR_DOWN;
+    move_dir <= DIR_RIGHT;
     xdelay <= 0;
     ydelay <= 0;
 end
@@ -77,27 +79,15 @@ begin
     ypos_nxt = ypos;
     xdelay_nxt = xdelay;
     ydelay_nxt = ydelay;
-    /*if(xpos >= X_MAX) xpos_nxt = X_MAX;
-    else if (xpos <= 1) xpos_nxt = 1;
-    else xpos_nxt = xpos;
-    if(ypos >= Y_MAX) ypos_nxt = Y_MAX;*/
-
-    /*case (key)
-        KEY_UP: state_nxt = GOING_UP;
-        KEY_DOWN: state_nxt = GOING_DOWN;
-        KEY_LEFT: state_nxt = GOING_LEFT;
-        KEY_RIGHT: state_nxt = GOING_RIGHT;
-        default: state_nxt = IDLE;
-    endcase*/
-
+    
     if(xtimer < xdelay) xtimer_nxt = xtimer + 1;
     else
     begin
         xtimer_nxt = 0;
         if(xdelay < DELAY_MAX)
         begin
-            if(move_dir == DIR_LEFT) xpos_nxt = (xpos <= 1) ? 0 : xpos - 1; //xpos_nxt = xpos - 1; // 
-            else if(move_dir == DIR_RIGHT) xpos_nxt = (xpos >= X_MAX) ? X_MAX : xpos + 1;  //xpos_nxt = xpos + 1; // 
+            if(move_dir == DIR_LEFT) xpos_nxt = (car_x_pos <= 48) ? 48 : xpos - 1;
+            else if(move_dir == DIR_RIGHT) xpos_nxt = (car_x_pos >= X_MAX) ? X_MAX : xpos + 1;
         end
     end
 
@@ -107,9 +97,64 @@ begin
         ytimer_nxt = 0;
         if(ydelay < DELAY_MAX)
         begin
-            if(move_dir == DIR_UP) ypos_nxt = (ypos <= 1) ? 0 : ypos - 1; //ypos_nxt = ypos - 1; // 
-            else if(move_dir == DIR_DOWN) ypos_nxt = (ypos >= Y_MAX) ? Y_MAX : ypos + 1; //ypos_nxt = ypos + 1; // 
+            if(move_dir == DIR_UP) ypos_nxt = (ypos <= 1) ? 1 : ypos - 1;
+            else if(move_dir == DIR_DOWN) ypos_nxt = (ypos >= Y_MAX) ? Y_MAX : ypos + 1; 
         end
+    end
+
+    if((car_x_pos >= 896 && car_y_pos <= 384) || (car_x_pos >= 48 && car_x_pos <= 64 && car_y_pos <= 96) || 
+        (car_x_pos >= 64 && car_x_pos <= 80 && car_y_pos <= 64) || (car_x_pos >= 80 && car_x_pos <= 96 && car_y_pos <= 48) || 
+        (car_x_pos >= 96 && car_x_pos <= 112 && car_y_pos <= 32) || (car_y_pos <= 16) || 
+        (car_x_pos >= 140  && car_x_pos <= 168 && car_y_pos >= 136 && car_y_pos <= 164) ||
+        (car_x_pos >= 764  && car_x_pos <= 792 && car_y_pos >= 136 && car_y_pos <= 164) ||
+        (car_x_pos >= 176  && car_x_pos <= 592 && car_y_pos >= 160 && car_y_pos <= 304) ||
+        (car_x_pos >= 480  && car_x_pos <= 540 && car_y_pos >= 332 && car_y_pos <= 352) ||
+        (car_x_pos >= 268  && car_x_pos <= 294 && car_y_pos >= 424 && car_y_pos <= 488) ||
+        (car_x_pos >= 324  && car_x_pos <= 380 && car_y_pos >= 488 && car_y_pos <= 512) ||
+        (car_x_pos >= 548  && car_x_pos <= 604 && car_y_pos >= 488 && car_y_pos <= 512) ||
+        (car_x_pos >= 740  && car_x_pos <= 796 && car_y_pos >= 488 && car_y_pos <= 512) ||
+        (car_x_pos >= 832  && car_x_pos <= 912 && car_y_pos >= 496 && car_y_pos <= 512) ||
+        (car_x_pos >= 336  && car_x_pos <= 784 && car_y_pos >= 592 && car_y_pos <= 608) ||
+        (car_x_pos >= 800  && car_x_pos <= 824 && car_y_pos >= 596 && car_y_pos <= 666) ||
+        (car_x_pos >= 304  && car_x_pos <= 784 && car_y_pos >= 656 && car_y_pos <= 672) ||
+        (car_x_pos >= 324  && car_x_pos <= 380 && car_y_pos >= 488 && car_y_pos <= 512) ||
+        (car_x_pos >= 50  && car_x_pos <= 234 && car_y_pos >= 720 && car_y_pos <= 752) ||
+        (car_x_pos >= 946  && car_x_pos <= 1008 && car_y_pos >= 730 && car_y_pos <= 752)) 
+        begin
+            if(move_dir == DIR_DOWN) begin
+                ypos_nxt = ypos - 1;
+            end
+            else if(move_dir == DIR_UP) begin
+                ypos_nxt = ypos + 1;
+            end
+            else if(move_dir == DIR_LEFT) begin
+                xpos_nxt = xpos + 1;
+            end
+            else if(move_dir == DIR_RIGHT) begin
+                xpos_nxt = xpos - 1;
+            end
+   
+       end
+    
+    if(move_dir == DIR_DOWN) begin
+        car_x_pos = xpos + 32;
+        car_y_pos = ypos + 54;
+    end
+    else if(move_dir == DIR_UP) begin
+        car_x_pos = xpos + 32;
+        car_y_pos = ypos + 10;
+    end
+    else if(move_dir == DIR_LEFT) begin
+        car_x_pos = xpos + 10;
+        car_y_pos = ypos + 32;
+    end
+    else if(move_dir == DIR_RIGHT) begin
+        car_x_pos = xpos + 54;
+        car_y_pos = ypos + 32;
+    end
+    else begin
+        car_x_pos = xpos + 32;
+        car_y_pos = ypos + 32;
     end
 
     case (key)
@@ -145,33 +190,5 @@ begin
         end
     endcase
 end
-  /*  if(timer >= 2000000) begin
-        timer_nxt = 0;
-        xpos_nxt  = xpos + xspeed;
-        ypos_nxt  = ypos + yspeed;
-        if(key && KEY_UP) 
-        begin
-            yspeed_nxt = yspeed - 1;
-            state_nxt = GOING_UP;
-        end
-        if(key && KEY_DOWN) 
-        begin
-            yspeed_nxt = yspeed + 1;
-            state_nxt = GOING_DOWN;
-        end
-        if(key && KEY_LEFT) begin
-            xspeed_nxt = xspeed - 1;
-            state_nxt = GOING_LEFT;
-        end 
-        if(key && KEY_RIGHT) 
-        begin
-            xspeed_nxt = xspeed + 1;
-            state_nxt = GOING_RIGHT;
-        end*/
-        /*if(xspeed > 0) xspeed_nxt = xspeed - 1;
-        else if(xspeed < 0) xspeed_nxt = xspeed + 1;
-        if(yspeed > 0) yspeed_nxt = yspeed - 1;
-        else if(yspeed < 0) yspeed_nxt = yspeed + 1;*/
-    //end
 
 endmodule
